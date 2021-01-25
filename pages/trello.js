d212d51c8f1a608a070fa18eec686491311cc783
jacob whitford-bender <jacob@xmentium.com>
2021-01-24 19:26:03 -0700

    gotta filter out archived columns

--- pages/trello.js
+++ pages/trello.js
@@ -12,12 +12,14 @@ const {
 
 const getListId = listToGetRecipesFrom => pipe([
 	get (K (true)) ('lists'),
+	map (filter (o => !o.closed)),
 	chain (find (o => o.name === listToGetRecipesFrom)),
 	chain (get (K (true)) ('id')),
 	fromMaybe (''),
 ]);
 
 const prop = p => o => o[p];
+const trace = s => {console.log(s); return s;};
 const getUrls = listNames => o => {
 	const listIds = map (l => getListId (l) (o)) (listNames)
 	return pipe([
